c9c895550865e366183ab0ec998f0ebe89ff3b8e
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-24 11:49:19 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 
-import AboutSection from "@/components/sections/core/AboutSection"
-import BlogSection from "@/components/sections/core/BlogSection"
-import TechnologySection from "@/components/sections/core/TechnologySection"
-import VehiclesSection from "@/components/sections/core/VehiclesSection"
-
-import StatsSection from "./stateSection/page"
-import Testride from "./Testride/page"
 
 
 
-import VehiclesSection from "@/app/bikes/page"
+import VehiclesSection from "./bikes/page"
+
+
+
+
 import HeroSection from "./heroSection/page"
+
 import WhyChooseSection from "./whyChooseUs/page"
-import ReadyToRide from "./readyToRide/page"
 
 
 
@@ -24,23 +21,12 @@ export default function HomePage() {
 
       
 
-{/*       
-      <VehiclesSection />
-      <TechnologySection />
-      <AboutSection />
-      <BlogSection /> */}
+
     <HeroSection />
     <WhyChooseSection />
 
       <VehiclesSection />
-      <TechnologySection />
-      <AboutSection />
-      <BlogSection />
-
- 
-
-    
-      <Testride />
+      
 
 
     </main>
