bf3fde2ebacbb2309b2deba5162ab83edf541a54
dipak560035 <dipaksah2070@gmail.com>
2026-06-05 11:23:37 +0545

    blog

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,4 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'ui-avatars.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 
 import type { NextConfig } from "next";
 
